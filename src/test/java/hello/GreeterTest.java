80be9ee3efed31a075bf003b3556c4ec35058bda
nareshkumarthota <nareshkumarthota1990@gmail.com>
2023-12-12 11:45:02 +0530

    changes

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -11,7 +11,7 @@ public class GreeterTest {
 
   @Test
   public void greeterSaysHello() {
-    assertThat(greeter.sayHello(), containsString("Hello123"));
+    assertThat(greeter.sayHello(), containsString("Hello12"));
   }
 
 }
