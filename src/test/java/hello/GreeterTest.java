cee42476d4625a54175ffadc15aae7d68d0e3be8
nareshkumarthota <nareshkumarthota1990@gmail.com>
2023-12-12 11:46:57 +0530

    branch 3 changes

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -11,7 +11,7 @@ public class GreeterTest {
 
   @Test
   public void greeterSaysHello() {
-    assertThat(greeter.sayHello(), containsString("Hello12"));
+    assertThat(greeter.sayHello(), containsString("Hello12-branch3"));
   }
 
 }
